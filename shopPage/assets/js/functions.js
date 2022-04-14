ad2955ec1dbe7ebd36badfac43259e1418c2a753
TowhozibAhmed <79678618+TowhozibAhmed@users.noreply.github.com>
2022-04-14 17:45:41 +0600

    New Code Push

---
+++ shopPage/assets/js/functions.js
@@ -0,0 +1,53 @@
+
+function sendData(key, dataName) {
+
+    let productDataStore = JSON.stringify(dataName);
+    localStorage.setItem(key, productDataStore)
+
+    return true;
+
+}
+
+
+function getData(key) {
+
+    let dataName = localStorage.getItem(key);
+
+    return dataName ? JSON.parse(dataName) : false;
+
+
+}
+
+
+// function allProducts() {
+
+//     productLists.innerHTML = ''
+
+//     let allData = ''
+
+//     productDatas.map(pdata => {
+
+//         allData += ` 
+            // <div class="col-md-4">
+            //     <div class="single__product__card card card-body">
+            //         <div class="single__product__card__thumb">
+            //             <img src="${pdata.productPhoto}"
+            //                 alt="Product Image">
+            //         </div>
+            //         <div class="single__product__card__content">
+            //             <h4>${pdata.name}</h4> 
+            //             <p><b><del> ${pdata.productPrice}</del></b>   <b>${pdata.productSalePrice}</b></p> 
+            //             <div class="card__btn pt-2">
+            //                 <button class="btn btn-success">Add to Cart</button>
+            //             </div>
+            //         </div>
+            //     </div>
+            // </div>
+//        `
+//     })
+
+
+//     productLists.innerHTML = allData
+
+
+// }
--- shopPage/assets/js/main.js
+++ shopPage/assets/js/main.js
@@ -1,78 +1,171 @@
-const todoForm = document.getElementById('todoForm');
-let todoList = document.getElementById('todoList')
+const productForm = document.getElementById('productForm');
+let addProduct = document.getElementById('addProduct');
+let publish = document.getElementById('publish');
+let closeBtn = document.getElementById('closeBtn');
+let productLists = document.getElementById('productLists');
+let clearProducts = document.getElementById('clearProducts');
+let addProductWrapper = document.querySelector('.addProduct__wrapper');
 
+addProduct.addEventListener('click', () => {
 
-todoForm.addEventListener('submit', (e) => {
-    e.preventDefault()
+    addProductWrapper.style.display = 'block'
 
-    let todoInput = document.getElementById('todoInput').value;
-    let todoSelect = document.getElementById('todoSelect').value;
+})
 
-    if (todoInput == '' || todoSelect == '') {
-        alert('All Fields Are Required');
-    } else {
- 
-        let li = document.createElement('li');
-        li.className = 'list-group-item d-flex align-items-center justify-content-between';
-        li.innerHTML = todoInput;
-        todoInput = ' ';
-        todoList.appendChild(li)
+closeBtn.addEventListener('click', () => {
 
-        let closeBtn = document.createElement('button');
-        closeBtn.className = ('close')
-        closeBtn.innerHTML = '&times;'
-        li.appendChild(closeBtn)
+    addProductWrapper.style.display = 'none'
 
-        let importance = document.createElement('p')
+})
 
-        li.appendChild(importance)
-        importance.innerHTML = todoSelect;
-        li.insertBefore(importance, closeBtn)
- 
-        closeBtn.addEventListener('click', () => {
+clearProducts.addEventListener('click', () => {
 
-            li.remove()
-
-        })
-    }
-
+    localStorage.clear();
+    allProducts()
 
 })
 
 
-// const todoForm = document.getElementById('todoForm');
-// const todoList = document.getElementById('todoList');
-// const addTodo = document.getElementById('addTodo');
+productForm.addEventListener('submit', (e) => {
 
-// let todoListData = []
-// let closebtn;
+    e.preventDefault();
 
-// addTodo.addEventListener('click', (e) => {
+    let productName = document.getElementById('productName').value;
+    let productPrice = document.getElementById('productPrice').value;
+    let productSalePrice = document.getElementById('productSalePrice').value;
+    let productImage = document.getElementById('productImage').value;
+
+
+
+    if (productName == '' || productPrice == '' || productSalePrice == '' || productImage == '') {
+        alert('All Fields Are Required!!')
+    } else {
+
+
+        let productDataArray;
+        let productDataGet = getData('Product-Details')
+
+
+        if (productDataGet) {
+            productDataArray = productDataGet;
+        } else {
+            productDataArray = []
+        }
+
+        productDataArray.push({
+            name: productName,
+            productPrice: productPrice,
+            productSalePrice: productSalePrice,
+            productImage: productImage,
+        })
+
+
+        sendData('Product-Details', productDataArray);
+        allProducts()
+
+    }
+
+})
+
+allProducts()
+
+function allProducts() {
+
+    productLists.innerHTML = ``
+
+
+
+    let productDataStore = getData('Product-Details')
+
+
+
+    productDataStore.map((pdata) => {
+        productLists.innerHTML += `
+        <div class="col-md-4">
+            <div class="single__product__card card card-body">
+                <div class="single__product__card__thumb">
+                    <img src="${pdata.productImage}"
+                        alt="Product Image">
+                </div>
+                <div class="single__product__card__content">
+                    <h4>${pdata.name}</h4> 
+                    <p><b><del> ${pdata.productPrice}</del></b>   <b>${pdata.productSalePrice}</b></p> 
+                    <div class="card__btn pt-2">
+                        <button class="btn btn-success">Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+      `
+    })
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+// let productDatas = getData('Product-Details')
+
+// productForm.addEventListener('submit', (e) => {
 //     e.preventDefault()
-//     let todoInput = document.getElementById('todoInput');
-//     let todoSelect = document.getElementById('todoSelect');
-
-//     todoListData.push(todoInput.value);
 
 
-//     todoList.innerHTML = ''
 
-//     todoListData.map((data) => {
-
-//         todoList.innerHTML += `<li class="list-group-item d-flex align-items-center justify-content-between">${data} <button class="close" id="close">&times;</button></li>`
-
-//     })
+//     if (productName.value == '' || productPrice.value == '' || productSalePrice.value == '' || productImage.value == '') {
+//         alert('All Fields are Required')
+//     } else {
 
 
+//         let productName = document.getElementById('productName');
+//         let productPrice = document.getElementById('productPrice');
+//         let productSalePrice = document.getElementById('productSalePrice');
+//         let productImage = document.getElementById('productImage');
+
+
+
+
+//         let productDataArray = [];
+//         if (productDatas) {
+//             productDataArray = productDatas;
+//         } else {
+//             productDataArray = []
+//         }
+
+
+//         productDataArray.push({
+//             name: productName.value,
+//             productPrice: productPrice.value,
+//             productSalePrice: productSalePrice.value,
+//             productPhoto: productImage.value
+//         })
+
+//         sendData('Product-Details', productDataArray)
+
+//         allProducts()
+
+
+
+//     }
 
 // })
 
 
+// allProducts()
 
 
-// const element = document.querySelectorAll('.list-group-item > .close');
+// let clearData = document.getElementById('clearProducts');
 
+// clearData.addEventListener('click', () => {
 
-// element.addEventListener('click', () => {
-//     console.log('sdfsdfgsgs')
+//     localStorage.clear()
+//     productLists.innerHTML = '';
+
 // })
